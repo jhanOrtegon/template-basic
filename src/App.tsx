2b7778b155a8c54532a8b822539416134d65ea4c
desarrolladortic4 <desarrolladortic4@ttncompany.com>
2025-06-12 16:30:23 -0500

    chore: initial commit with ESLint strict config

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function AppRRrDD() {
+function AppR() {
   const [count, setCount] = useState(0)
 
   return (
