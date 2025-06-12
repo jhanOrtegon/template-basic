6e312837155c50fc56141c55ad6d49d533817928
desarrolladortic4 <desarrolladortic4@ttncompany.com>
2025-06-12 16:43:25 -0500

    chore: initial commit with ESLint strict config

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,3 +1,9 @@
-mkdir -p .husky/_
-echo -e '#!/bin/sh\n\nif [ -z "$husky_skip_init" ]; then\n  debug () {\n    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"\n  }\n\n  readonly hook_name="$(basename "$0")"\n  debug "starting $hook_name..."' > .husky/_/husky.sh
-chmod +x .husky/_/husky.sh
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
+#!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
+
+They WILL FAIL in v10.0.0
+"
